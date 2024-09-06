a7e8d03b97a03ed63fc66dda8f78784c2695559b
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2024-09-06 15:58:44 +0530

    done

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def main():
     st.markdown("""
     <style>
     .main {
-        background-color: darkturquoise;
+        background-color: rgb(137, 79, 31);
     }
     /* Background image for the whole page */
     .css-1f3v6nr {

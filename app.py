22647e59ffa78321b7bf1d83d226c05fb2221a30
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2024-09-06 23:42:44 +0530

    done

--- app.py
+++ app.py
@@ -179,7 +179,7 @@ def main():
 
     if choice == "🏠 Home":
         st.subheader("🏠 Home")
-        st.subheader.markdown("""
+        st.markdown("""
     <style>
     .css-1vhystk p, .css-1vhystk ul, .css-1vhystk li, .css-1vhystk h3 {
         color: #191970 !important;

024f9af95f0b1f0e899a054765027df8033e4e19
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2024-09-07 01:08:42 +0530

    done

--- app.py
+++ app.py
@@ -111,8 +111,7 @@ def main():
 
     /* Change button and box active color */
     button:active, .stButton>button:focus {
-        background-color: #009688 !important;
-        color: black !important;  /* Changed text color */
+        background-color: #009688 !important;  /* Changed text color */
         animation: fadeIn 2s ease-in; /* Added animation */
     }
 

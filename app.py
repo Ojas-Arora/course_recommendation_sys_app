289f9fa8cda514cd8cc31adaec9aa700e8a472c0
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2024-09-07 00:16:38 +0530

    done

--- app.py
+++ app.py
@@ -268,7 +268,7 @@ def main():
         to { opacity: 1; }
     }
     </style>
-    <h2 class="h2">Welcome to the Course Recommendation App🚀</strong></h2>
+    <h2 class="h2">Welcome to the <strong>Course Recommendation App🚀</strong></h2>
 
     <h3 class="custom-header">🎯 <strong>Objective:</strong></h3>
     <p>This app is designed to help you discover the best courses that match your learning interests. With an extensive collection of courses, our goal is to provide personalized recommendations to guide your educational journey.</p>

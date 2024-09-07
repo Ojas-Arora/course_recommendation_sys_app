10f9613353cb3e57335fe09aaa239ea630c94148
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2024-09-07 13:33:44 +0530

    done

--- app.py
+++ app.py
@@ -209,12 +209,11 @@ def main():
         st.dataframe(df.head(10))
     
     elif choice == "🔍 Recommend":
-        st.markdown(
+         st.markdown(
         '<h3 style="color:#191970;">🔍 Recommend Courses</h3>',
         unsafe_allow_html=True
     )
-    
-        st.markdown("""
+    st.markdown("""
         <style>
         .custom-header {
             color: #191970;

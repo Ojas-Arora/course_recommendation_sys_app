05a0a346622bce471407445de660e6c92936d279
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2024-09-07 17:17:54 +0530

    done

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def get_recommendation(title, cosine_sim_mat, df, num_of_rec=10):
 RESULT_TEMP = """
 <div style="width:100%;height:100%;margin:5px;padding:10px;position:relative;border-radius:10px;
 box-shadow:0 0 10px 2px #191970; background-color: #ffffff; border-left: 5px solid #191970; margin-bottom: 20px;">
-<h4 style="color:#009688;">{}</h4>
+<h4 style="color:#191970;">{}</h4>
 <p style="color:#191970;"><span style="color:#333;">🔍 Similarity Score:</span> {}</p>
 <p style="color:#191970;"><span style="color:#333;">🔗</span> <a href="{}" target="_blank">Course Link</a></p>
 <p style="color:#191970;"><span style="color:#333;">💰 Price:</span> {}</p>
